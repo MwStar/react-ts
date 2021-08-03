11edad668e0f7d1bf8f6bc2cf8a4efbd1768193c
梁丽 <liangli@evercreative.com.cn>
2021-08-03 10:59:07 +0800

    chora: add commitlint to force commit style

---
+++ src/dist/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var add = function (a, b) {
+    console.log(a + b);
+    return a + b;
+};
+var a = 1;
+exports["default"] = add;
---
+++ src/hello.ts
@@ -0,0 +1,7 @@
+const add = (a, b) => {
+  console.log(a + b);
+  return a + b;
+};
+const a = 1;
+
+export default add;
